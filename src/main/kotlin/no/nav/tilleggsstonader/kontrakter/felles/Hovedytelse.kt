89935432176602e214f5c7a98387fece94ba3c1e
Johan Blomgren <blommish@users.noreply.github.com>
2023-09-26 09:34:39 +0200

    ✨ Søknadsskjema for Barnetilsyn (#4)
    * Søknadsskjema for Barnetilsyn
    
    * Vi har bestemt oss for å bruke ident i stedet for personIdent

---
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/Hovedytelse.kt
@@ -0,0 +1,14 @@
+package no.nav.tilleggsstonader.kontrakter.felles
+
+enum class Hovedytelse {
+    AAP,
+    OVERGANGSSTØNAD,
+    GJENLEVENDEPENSJON,
+    DAGPENGER,
+    TILTAKSPENGER,
+    KVALIFIKASJONSPROGRAMMET,
+    INTRODUKSJONSPROGRAMMET,
+    SYKEPENGER,
+    UFØRETRYGD,
+    INGEN_PENGESTØTTE,
+}
---
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/søknad/barnetilsyn/SøknadsskjemaBarnetilsyn.kt
@@ -0,0 +1,32 @@
+package no.nav.tilleggsstonader.kontrakter.søknad.barnetilsyn
+
+import no.nav.tilleggsstonader.kontrakter.felles.Hovedytelse
+import no.nav.tilleggsstonader.kontrakter.søknad.EnumFelt
+import no.nav.tilleggsstonader.kontrakter.søknad.JaNei
+
+data class SøknadsskjemaBarnetilsyn(
+    val hovedytelse: EnumFelt<Hovedytelse>,
+    val aktivitet: Aktivitet,
+    val barn: List<BarnMedBarnepass>,
+)
+
+data class Aktivitet(
+    val utdanning: EnumFelt<JaNei>,
+)
+
+data class BarnMedBarnepass(
+    val ident: String,
+    val type: EnumFelt<TypeBarnepass>,
+    val startetIFemte: EnumFelt<JaNei>?,
+    val årsak: EnumFelt<ÅrsakBarnepass>?,
+)
+
+enum class TypeBarnepass {
+    BARNEHAGE_SFO_AKS,
+    ANDRE,
+}
+
+enum class ÅrsakBarnepass {
+    TRENGER_MER_PASS_ENN_JEVNALDRENDE,
+    MYE_BORTE_ELLER_UVANLIG_ARBEIDSTID,
+}
