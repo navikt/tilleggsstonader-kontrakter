e825517c4c97d16d3d8cb89ae079c3ff8f2ec223
eirarset <eirik.arseth@nav.no>
2023-10-30 00:56:29 +0100

    Automatisk journalføringsrequest trenger ikke mappe eller prioritet
    ts-sak har ansvaret for å opprette oppgaver, og evt. oppgaveprioritet. soknad-api trenger derfor ikke noe forhold til dette.

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/sak/journalføring/AutomatiskJournalføring.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/sak/journalføring/AutomatiskJournalføring.kt
@@ -1,15 +1,12 @@
 package no.nav.tilleggsstonader.kontrakter.sak.journalføring
 
 import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
-import no.nav.tilleggsstonader.kontrakter.oppgave.OppgavePrioritet
 import java.util.UUID
 
 data class AutomatiskJournalføringRequest(
     val personIdent: String,
     val journalpostId: String,
     val stønadstype: Stønadstype,
-    val mappeId: Long?,
-    val prioritet: OppgavePrioritet = OppgavePrioritet.NORM,
 )
 
 data class AutomatiskJournalføringResponse(
