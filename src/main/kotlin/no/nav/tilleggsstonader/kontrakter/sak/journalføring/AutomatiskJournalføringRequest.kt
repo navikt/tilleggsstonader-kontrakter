8f8218c4f23c47f1708591e9c7f7a7be9b846dd6
Johan Blomgren <blommish@users.noreply.github.com>
2023-12-15 09:37:45 +0100

    Fjerner response som ikke brukes til noe (#26)

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/sak/journalføring/AutomatiskJournalføringRequest.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/sak/journalføring/AutomatiskJournalføringRequest.kt
@@ -1,15 +1,9 @@
 package no.nav.tilleggsstonader.kontrakter.sak.journalføring
 
 import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
-import java.util.UUID
 
 data class AutomatiskJournalføringRequest(
     val personIdent: String,
     val journalpostId: String,
     val stønadstype: Stønadstype,
-)
-
-data class AutomatiskJournalføringResponse(
-    val fagsakId: UUID,
-    val behandlingId: UUID?,
 )
