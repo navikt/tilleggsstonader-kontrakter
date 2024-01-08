c92c68bf85d3c82d7498b3b196cb007e4458b7fe
sarahjelle <sara.hjelle@bekk.no>
2024-01-08 11:14:31 +0100

    Oppdater data class for JournalposterForBrukerRequest

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/journalpost/JournalposterForBrukerRequest.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/journalpost/JournalposterForBrukerRequest.kt
@@ -4,7 +4,8 @@ import no.nav.tilleggsstonader.kontrakter.felles.Tema
 
 data class JournalposterForBrukerRequest(
     val brukerId: Bruker,
-    val antall: Int,
     val tema: List<Tema>? = null,
-    val journalposttype: List<Journalposttype>? = null,
+    val journalposttype: Journalposttype? = null,
+    val journalstatus: Journalstatus?,
+    val antall: Int = 200,
 )
