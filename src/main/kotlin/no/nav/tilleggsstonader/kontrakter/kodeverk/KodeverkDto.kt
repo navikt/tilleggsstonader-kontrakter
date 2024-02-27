0a408b63334ac6ea86528cb8a01c432efc4d0cb3
Johan Blomgren <blommish@users.noreply.github.com>
2024-02-27 14:19:43 +0100

    Kopiert dto for kodeverk som skal brukes av søknad-api og sak for å mappe postnummer til poststed (#53)

---
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/kodeverk/KodeverkDto.kt
@@ -0,0 +1,52 @@
+package no.nav.tilleggsstonader.kontrakter.kodeverk
+
+import java.time.LocalDate
+
+data class KodeverkDto(val betydninger: Map<String, List<BetydningDto>>)
+
+data class BetydningDto(
+    val gyldigFra: LocalDate,
+    val gyldigTil: LocalDate,
+    val beskrivelser: Map<String, BeskrivelseDto>,
+)
+
+data class BeskrivelseDto(
+    val term: String,
+    val tekst: String,
+)
+
+enum class KodeverkSpråk(val kode: String) {
+    BOKMÅL("nb"),
+}
+
+private fun LocalDate.mellom(fra: LocalDate, til: LocalDate) =
+    this.isEqual(fra) || this.isEqual(til) || (this.isAfter(fra) && this.isBefore(til))
+
+/**
+ * @param sisteGjeldende henter siste gjeldende verdi for en gitt kode hvis den mangler for gitt dato
+ */
+fun KodeverkDto.hentGjeldende(
+    kode: String,
+    gjeldendeDato: LocalDate = LocalDate.now(),
+    språk: KodeverkSpråk = KodeverkSpråk.BOKMÅL,
+    sisteGjeldende: Boolean = false,
+): String? {
+    val betydningForKode = betydninger[kode] ?: return null
+    var betydning = betydningForKode.firstOrNull { gjeldendeDato.mellom(it.gyldigFra, it.gyldigTil) }
+    if (betydning == null && sisteGjeldende) {
+        betydning = betydningForKode.maxByOrNull { it.gyldigFra }
+    }
+    return betydning?.beskrivelser?.get(språk.kode)?.term
+}
+
+fun KodeverkDto.mapTerm(): Map<String, String> {
+    return betydninger.mapValues {
+        if (it.value.isEmpty()) {
+            ""
+        } else if (it.value.size != 1) {
+            this.hentGjeldende(it.key) ?: ""
+        } else {
+            it.value.singleOrNull()?.beskrivelser?.get(KodeverkSpråk.BOKMÅL.kode)?.term ?: ""
+        }
+    }
+}
