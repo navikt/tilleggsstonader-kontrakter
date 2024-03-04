e9df7384a6ccc6548c9167398a6282369c4ef99a
Johan Blomgren <blommish@users.noreply.github.com>
2024-03-04 12:40:01 +0100

    Fjernet deprecated søknadsskjema for barnetilsyn

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/søknad/barnetilsyn/AvsnittBarnetilsyn.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/søknad/barnetilsyn/AvsnittBarnetilsyn.kt
@@ -6,10 +6,6 @@ import no.nav.tilleggsstonader.kontrakter.søknad.EnumFlereValgFelt
 import no.nav.tilleggsstonader.kontrakter.søknad.JaNei
 import no.nav.tilleggsstonader.kontrakter.søknad.TekstFelt
 
-// TODO slett når sak og søknad er merget
-@Deprecated("Flyttet", ReplaceWith("no.nav.tilleggsstonader.kontrakter.søknad.SøknadsskjemaBarnetilsyn"))
-typealias SøknadsskjemaBarnetilsyn = no.nav.tilleggsstonader.kontrakter.søknad.SøknadsskjemaBarnetilsyn
-
 data class HovedytelseAvsnitt(
     val hovedytelse: EnumFlereValgFelt<Hovedytelse>,
     val boddSammenhengende: EnumFelt<JaNei>?,
