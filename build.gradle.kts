fdcc04fc373b7bf924b95172ec7fa79496a4b6e2
Johan Blomgren <blommish@users.noreply.github.com>
2024-03-07 06:38:09 +0100

    Endrer mappeId til å være optional for å kunne sende den som null/verdi/ikke sendes med (#59)
    For å kunne nullstille mappeId når den skal tas ut av vent

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-core")
     implementation("com.fasterxml.jackson.core:jackson-annotations")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     testImplementation("org.junit.jupiter:junit-jupiter")
--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/ObjectMapperProvider.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/ObjectMapperProvider.kt
@@ -3,6 +3,7 @@ package no.nav.tilleggsstonader.kontrakter.felles
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.datatype.jsr310.deser.YearMonthDeserializer
 import com.fasterxml.jackson.module.kotlin.addDeserializer
@@ -15,6 +16,7 @@ object ObjectMapperProvider {
     val objectMapper: ObjectMapper = // Jackson2ObjectMapperBuilder.json().build()
         ObjectMapper()
             .registerKotlinModule()
+            .registerModule(Jdk8Module())
             .registerModule(
                 JavaTimeModule()
                     .addDeserializer(
