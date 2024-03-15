d09f4907dc92a52edee0a9935001f478af42bc55
Johan Blomgren <blommish@users.noreply.github.com>
2024-03-15 13:56:55 +0100

    Kontrakt for å kunne hente aktiviteter fra arena (#63)

---
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/aktivitet/StatusAktivitet.kt
@@ -0,0 +1,21 @@
+package no.nav.tilleggsstonader.kontrakter.aktivitet
+
+/**
+ * Statuser mappet fra statuser i arena i integrasjoner
+ * [no.nav.tilleggsstonader.integrasjoner.arena.dto.StatusAktivitetArena]
+ */
+enum class StatusAktivitet(val beskrivelse: String, val rettTilÅSøke: Boolean) {
+    VENTER_PA_OPPSTART("Venter på oppstart", true),
+    AKTUELL("Aktuell", true),
+    DELTAR("Deltar", true),
+    AVBRUTT("Avbrutt", true),
+    FULLFØRT("Fullført", true),
+    OPPHØRT("Opphørt", true),
+    OVERFØRT("Overført", true),
+    BEHOV("Behov", true),
+
+    IKKE_AKTUELL("Ikke aktuell", false),
+    FEILREGISTRERT("Feilregistrert", false),
+    PLANLAGT("Planlagt", false),
+    VENTELISTE("Venteliste", false),
+}
