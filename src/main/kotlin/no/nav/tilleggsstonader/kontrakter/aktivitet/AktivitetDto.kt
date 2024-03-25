f1d48a4c32ddf3ed6a904c779554ac77d0163ef8
Johan Blomgren <blommish@users.noreply.github.com>
2024-03-25 09:55:44 +0100

    Det finnes flere typer enn inneholder fler typer enn hva jeg har tilgang til, så typer opp med string og navn i stedet for å unngå mapping til enum-type som ikke finnes

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/aktivitet/AktivitetDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/aktivitet/AktivitetDto.kt
@@ -2,11 +2,15 @@ package no.nav.tilleggsstonader.kontrakter.aktivitet
 
 import java.time.LocalDate
 
-data class AktivitetDto(
+/**
+ * @param type fra arena er ikke mappet til [TypeAktivitet] fordi [TypeAktivitet] ikke er komplett
+ */
+data class AktivitetArenaDto(
     val id: String,
     val fom: LocalDate?,
     val tom: LocalDate?,
-    val type: TypeAktivitet,
+    val type: String,
+    val typeNavn: String,
     val status: StatusAktivitet?,
     val antallDagerPerUke: Int?,
     val prosentDeltakelse: Float?,
