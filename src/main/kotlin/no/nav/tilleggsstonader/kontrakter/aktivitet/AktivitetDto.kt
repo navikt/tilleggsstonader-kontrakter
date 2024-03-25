a99a8200b5e35ff72318a710fd329811e20fb084
Johan Blomgren <blommish@users.noreply.github.com>
2024-03-25 09:58:22 +0100

    Kontrakt hente aktivitet fra arena (#65)
    * Det finnes flere typer enn inneholder fler typer enn hva jeg har tilgang til, så typer opp med string og navn i stedet for å unngå mapping til enum-type som ikke finnes
    
    * Legger til info om aktiviteten gir rett på stønad eller om det gjelder utdanning

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/aktivitet/AktivitetDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/aktivitet/AktivitetDto.kt
@@ -1,15 +1,23 @@
 package no.nav.tilleggsstonader.kontrakter.aktivitet
 
+import java.math.BigDecimal
 import java.time.LocalDate
 
-data class AktivitetDto(
+/**
+ * @param type fra arena er ikke mappet til [TypeAktivitet] fordi [TypeAktivitet] ikke er komplett
+ */
+data class AktivitetArenaDto(
     val id: String,
     val fom: LocalDate?,
     val tom: LocalDate?,
-    val type: TypeAktivitet,
+    val type: String,
+    val typeNavn: String,
     val status: StatusAktivitet?,
+    val statusArena: String?,
     val antallDagerPerUke: Int?,
-    val prosentDeltakelse: Float?,
+    val prosentDeltakelse: BigDecimal?,
+    val erStønadsberettiget: Boolean?,
+    val erUtdanning: Boolean?,
     val arrangør: String?,
     val kilde: Kilde,
 )
