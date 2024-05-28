b59087562e1a4f41ed128d730d8f473f21167164
Johan Blomgren <blommish@users.noreply.github.com>
2024-05-28 09:13:58 +0200

    Lagt til request for å hente ytelse og endrer response til å inneholde en liste med de ønskede typene med status for hver type

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/ytelse/YtelsePerioderDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/ytelse/YtelsePerioderDto.kt
@@ -1,6 +1,48 @@
 package no.nav.tilleggsstonader.kontrakter.ytelse
 
+import java.time.LocalDate
+
+/**
+ * TODO slett default verdier når den er tatt i bruk av consumer
+ * @param typer definierer hvilke typer som skal hentes
+ */
+data class YtelsePerioderRequest(
+    val ident: String,
+    val fom: LocalDate = LocalDate.now(),
+    val tom: LocalDate = LocalDate.now(),
+    val typer: List<TypeYtelsePeriode> = TypeYtelsePeriode.entries,
+)
+
+/**
+ * TODO slett [arbeidsavklaringspenger]
+ * TODO slett [ensligForsørger]
+ * TODO slett default emptylist
+ * Når perioder og hentetInformasjon er tatt i bruk
+ */
 data class YtelsePerioderDto(
     val arbeidsavklaringspenger: PerioderArbeidsavklaringspenger,
     val ensligForsørger: PerioderEnsligForsørger,
+    val perioder: List<YtelsePeriode> = emptyList(),
+    val hentetInformasjon: List<HentetInformasjon> = emptyList(),
 )
+
+data class YtelsePeriode(
+    val type: TypeYtelsePeriode,
+    val fom: LocalDate,
+    val tom: LocalDate,
+)
+
+data class HentetInformasjon(
+    val type: TypeYtelsePeriode,
+    val status: StatusHentetInformasjon,
+)
+
+enum class StatusHentetInformasjon {
+    OK,
+    FEILET,
+}
+
+enum class TypeYtelsePeriode {
+    AAP,
+    ENSLIG_FORSØRGER,
+}
