2f1bfb043c6c6e7e01e19a19e7dc6271005dfd1f
Eirik Årseth <eirik.arseth@nav.no>
2024-06-13 16:12:46 +0200

    Skal bruke arkivtema ved henting av vedlegg

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/journalpost/JournalposterForBrukerRequest.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/journalpost/JournalposterForBrukerRequest.kt
@@ -1,10 +1,10 @@
 package no.nav.tilleggsstonader.kontrakter.journalpost
 
-import no.nav.tilleggsstonader.kontrakter.felles.Tema
+import no.nav.tilleggsstonader.kontrakter.felles.Arkivtema
 
 data class JournalposterForBrukerRequest(
     val brukerId: Bruker,
-    val tema: List<Tema> = emptyList(),
+    val tema: List<Arkivtema> = emptyList(),
     val journalposttype: List<Journalposttype> = emptyList(),
     val journalstatus: List<Journalstatus> = emptyList(),
     val antall: Int = 200,
