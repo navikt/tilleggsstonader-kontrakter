e0b3cc1bb8f9d937830e7aafdce590848829ad0d
Johan Blomgren <blommish@users.noreply.github.com>
2024-08-19 11:29:05 +0200

    Fjerner målgruppe då det egentlige var stønadstype vi ønsket, men den informasjonen har vi ikke direkt, den går muligens å utlede fra kommentaren i noen tilfeller Fjerner frist då den alltid settes til samme dato

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/arena/oppgave/ArenaOppgaveDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/arena/oppgave/ArenaOppgaveDto.kt
@@ -1,22 +1,16 @@
 package no.nav.tilleggsstonader.kontrakter.arena.oppgave
 
-import no.nav.tilleggsstonader.kontrakter.arena.sak.Målgruppe
-import java.time.LocalDate
 import java.time.LocalDateTime
 
 /**
  * @param benk settes hvis en oppgave er tildelt. Mapping av verdier gjøres i tilleggsstonader-arena
  * @param tildelt er Arena-ident til saksbehandler
- *
- * @param målgruppe eksisterer ikke på alle type oppgaver. Finnes blant annet på søknader som blitt automatisk journalførte
  */
 data class ArenaOppgaveDto(
     val id: Long,
     val tittel: String,
     val kommentar: String,
-    val fristFerdigstillelse: LocalDate,
     val benk: String?,
     val tildelt: String?,
     val opprettetTidspunkt: LocalDateTime,
-    val målgruppe: Målgruppe?,
 )
