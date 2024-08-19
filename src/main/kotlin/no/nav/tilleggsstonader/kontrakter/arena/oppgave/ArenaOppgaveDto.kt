06cf66bacb4740a90c13ea5e9b637280335e62fe
Johan Blomgren <blommish@users.noreply.github.com>
2024-08-19 10:47:51 +0200

    Lagt til id på ArenaOppgaveDto

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/arena/oppgave/ArenaOppgaveDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/arena/oppgave/ArenaOppgaveDto.kt
@@ -11,6 +11,7 @@ import java.time.LocalDateTime
  * @param målgruppe eksisterer ikke på alle type oppgaver. Finnes blant annet på søknader som blitt automatisk journalførte
  */
 data class ArenaOppgaveDto(
+    val id: Long,
     val tittel: String,
     val kommentar: String,
     val fristFerdigstillelse: LocalDate,
