977a3288535e7258e496b1fbfc12d82f46b8511a
Nibedita.Pahari <nibedita.pahari@nav.no>
2024-09-19 13:07:49 +0200

    Added Læremidler in DocumentBrevkode Enum Class
    Co-authored-by: Runar Skagestad <runar.skagestad@nav.no>

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/sak/DokumentBrevkode.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/sak/DokumentBrevkode.kt
@@ -2,6 +2,7 @@ package no.nav.tilleggsstonader.kontrakter.sak
 
 enum class DokumentBrevkode(val verdi: String) {
     BARNETILSYN("NAV 11-12.15"),
+    LÆREMIDLER("NAV 11-12.16"),
     ;
 
     companion object {
