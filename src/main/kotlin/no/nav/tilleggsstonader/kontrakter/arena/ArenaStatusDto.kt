84edc6f1c20703367fcc4d893fa2f88ec2028065
Johan Blomgren <blommish@users.noreply.github.com>
2024-10-01 17:07:46 +0200

    info om det finnes innvilget vedtak i Arena

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/arena/ArenaStatusDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/arena/ArenaStatusDto.kt
@@ -13,12 +13,14 @@ data class SakStatus(
 
 /**
  * @param harVedtak finnes vedtak med/uten utfall. Uten ufall kan være at det ikke blir innstillt ennå
+ * @param harInnvilgetVedtak finnes innvilget vedtak
  * @param harAktivtVedtak har vedtak med utfall JA med tom > dagens dato
  * @param harVedtakUtenUtfall har vedtak uten utfall, dvs vedtak som ikke fått utfall ennå
  * @param vedtakTom maks-tom for vedtak med utfall JA
  */
 data class VedtakStatus(
     val harVedtak: Boolean,
+    val harInnvilgetVedtak: Boolean,
     val harAktivtVedtak: Boolean,
     val harVedtakUtenUtfall: Boolean,
     val vedtakTom: LocalDate? = null,
