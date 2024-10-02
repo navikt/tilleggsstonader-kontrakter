c002216323eb081c5a71de2dae6a0244f660067a
Johan Blomgren <blommish@users.noreply.github.com>
2024-10-02 16:11:01 +0200

    Endrer brevmottakere til sealed interface for å enklere kunne mappe ting i sak

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/brevmottaker/BrevmottakerDto.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/brevmottaker/BrevmottakerDto.kt
@@ -1,20 +1,24 @@
 package no.nav.tilleggsstonader.kontrakter.brevmottaker
 
-import java.util.*
+import java.util.UUID
+
+sealed interface BrevmottakerDto {
+    val id: UUID
+}
 
 data class BrevmottakerPersonDto(
-    val id: UUID,
+    override val id: UUID,
     val personIdent: String,
     val navn: String?,
     val mottakerRolle: MottakerRolle,
-)
+) : BrevmottakerDto
 
 data class BrevmottakerOrganisasjonDto(
-    val id: UUID,
+    override val id: UUID,
     val organisasjonsnummer: String,
     val organisasjonsnavn: String,
     val navnHosOrganisasjon: String,
-)
+) : BrevmottakerDto
 
 enum class MottakerRolle {
     BRUKER,
