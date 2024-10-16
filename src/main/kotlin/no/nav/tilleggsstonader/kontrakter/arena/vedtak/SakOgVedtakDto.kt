b9dd714af48d17fb033da79e1fbc88c24a77dd63
Johan Blomgren <blommish@users.noreply.github.com>
2024-10-16 08:45:23 +0200

    DTO for å kunne hente vedtak og aktiviteter fra Arena

---
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/arena/vedtak/SakOgVedtakDto.kt
@@ -0,0 +1,47 @@
+package no.nav.tilleggsstonader.kontrakter.arena.vedtak
+
+import java.time.LocalDate
+
+/**
+ * Inneholder vedtak og saker til en person i Arena.
+ * Hvert vedtak er koblet til en sak og kan hentes fra map over saker.
+ * Saker er en separat del fordi en sak kan være koblet til flere vedtak
+ */
+data class SakOgVedtakDto(
+    val vedtak: List<VedtakDto>,
+    val saker: Map<Int, SakDto>,
+)
+
+data class SakDto(
+    val målgruppe: String?,
+    val aktivitet: AktivitetDto?,
+)
+
+data class AktivitetDto(
+    val aktivitetId: Int,
+    val type: String,
+    val status: String,
+    val beskrivelse: String?,
+    val gjelderUtdanning: Boolean,
+
+    val typekode: String,
+    val statuskode: String,
+)
+
+data class VedtakDto(
+    val type: String,
+    val status: String,
+    val rettighet: String,
+    val rettighetkode: Rettighet,
+    val fom: LocalDate?,
+    val tom: LocalDate?,
+    val totalbeløp: Int?,
+    val datoInnstillt: LocalDate?,
+    val utfall: String?,
+    val vedtakfakta: List<VedtakfaktaDto>,
+)
+
+data class VedtakfaktaDto(
+    val type: String,
+    val verdi: String?,
+)
