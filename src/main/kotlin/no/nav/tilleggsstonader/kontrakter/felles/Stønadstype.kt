4713c49b1c782b9aaa0a2b987b5238f61ab84feb
Johan Blomgren <blommish@users.noreply.github.com>
2025-02-04 13:22:50 +0100

    Legger til antall måneder som brukes for å hente grunnlag direkt på stønadstypen då den brukes på flere steder i koden

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/Stønadstype.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/Stønadstype.kt
@@ -2,10 +2,12 @@ package no.nav.tilleggsstonader.kontrakter.felles
 
 /**
  * @param tittel kan eks brukes i brev
+ * @param grunnlagAntallMånederBakITiden brukes for å vite hvor langt tilbake i tiden man skal hente grunnlag for
  */
 enum class Stønadstype(
     val tittel: String,
+    val grunnlagAntallMånederBakITiden: Int,
 ) {
-    BARNETILSYN("Barnetilsyn"),
-    LÆREMIDLER("Læremidler"),
+    BARNETILSYN(tittel = "Barnetilsyn", grunnlagAntallMånederBakITiden = 3),
+    LÆREMIDLER(tittel = "Læremidler", grunnlagAntallMånederBakITiden = 6),
 }
