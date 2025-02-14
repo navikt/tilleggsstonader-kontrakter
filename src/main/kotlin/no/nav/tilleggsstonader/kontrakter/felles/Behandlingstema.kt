8ac523f8ab3d6d453d99eb8f37dc8ec792359e59
Johan Blomgren <blommish@users.noreply.github.com>
2025-02-14 06:06:55 +0100

    Legger til funksjon for å utlede behandlingstema fra stønadstype for å kunne gjenbruke i sak

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/Behandlingstema.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/Behandlingstema.kt
@@ -6,17 +6,24 @@ import com.fasterxml.jackson.annotation.JsonValue
 enum class Behandlingstema(
     @JsonValue val value: String,
 ) {
-    TilsynBarn("ab0300"),
-    Læremidler("ab0292"),
+    TilsynBarn("ab0300"), // Tilsyn av barn tilleggsstønad
+    Læremidler("ab0292"), // Læremidler tilleggsstønad
+    Boutgifter("ab0286"), // Boutgifter tilleggsstønad
     Feilutbetaling("ab0006"),
     Tilbakebetaling("ab0007"),
     ;
 
     companion object {
-        private val behandlingstemaMap = values().associateBy(Behandlingstema::value) + values().associateBy { it.name }
+        private val behandlingstemaMap = entries.associateBy(Behandlingstema::value) + entries.associateBy { it.name }
 
         @JvmStatic
         @JsonCreator
         fun fromValue(value: String): Behandlingstema = behandlingstemaMap[value] ?: error("Fant ikke Behandlingstema for value=$value")
     }
 }
+
+fun Stønadstype.tilBehandlingstema(): Behandlingstema =
+    when (this) {
+        Stønadstype.BARNETILSYN -> Behandlingstema.TilsynBarn
+        Stønadstype.LÆREMIDLER -> Behandlingstema.Læremidler
+    }
