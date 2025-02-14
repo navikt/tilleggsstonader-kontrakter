a4aaa4423e64180db5b45d047a75367bfe9c57f4
Johan Blomgren <blommish@users.noreply.github.com>
2025-02-14 06:05:15 +0100

    Legger til visningsnavn for å kunne bruke i brev i klage og sak

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/Stønadstype.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/Stønadstype.kt
@@ -1,13 +1,19 @@
 package no.nav.tilleggsstonader.kontrakter.felles
 
 /**
- * @param tittel kan eks brukes i brev
+ * @param visningsnavn kan eks brukes i brev
  * @param grunnlagAntallMånederBakITiden brukes for å vite hvor langt tilbake i tiden man skal hente grunnlag for
  */
 enum class Stønadstype(
-    val tittel: String,
+    val visningsnavn: String,
     val grunnlagAntallMånederBakITiden: Int,
 ) {
-    BARNETILSYN(tittel = "Barnetilsyn", grunnlagAntallMånederBakITiden = 3),
-    LÆREMIDLER(tittel = "Læremidler", grunnlagAntallMånederBakITiden = 6),
+    BARNETILSYN(
+        visningsnavn = "stønad til pass av barn",
+        grunnlagAntallMånederBakITiden = 3,
+    ),
+    LÆREMIDLER(
+        visningsnavn = "støtte til læremidler",
+        grunnlagAntallMånederBakITiden = 6,
+    ),
 }
