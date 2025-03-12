1128e89f928507c37addd75da905ace9ec4aa7d2
Johan Blomgren <blommish@users.noreply.github.com>
2025-03-12 09:24:30 +0100

    Legger inn stønadstype for dokumentbrevkode for å enkelt kunne finne hvilken stønadstype et dokument gjelder

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/sak/DokumentBrevkode.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/sak/DokumentBrevkode.kt
@@ -1,16 +1,19 @@
 package no.nav.tilleggsstonader.kontrakter.sak
 
+import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
+
 enum class DokumentBrevkode(
     val verdi: String,
+    val stønadstype: Stønadstype,
 ) {
-    BARNETILSYN("NAV 11-12.15"),
-    LÆREMIDLER("NAV 11-12.16"),
-    BOUTGIFTER("NAV 11-12.19"),
+    BARNETILSYN("NAV 11-12.15", Stønadstype.BARNETILSYN),
+    LÆREMIDLER("NAV 11-12.16", Stønadstype.LÆREMIDLER),
+    BOUTGIFTER("NAV 11-12.19", Stønadstype.BOUTGIFTER),
     ;
 
     companion object {
-        fun erGyldigBrevkode(brevKode: String?): Boolean = values().any { it.verdi == brevKode }
+        fun erGyldigBrevkode(brevKode: String?): Boolean = entries.any { it.verdi == brevKode }
 
-        fun fraBrevkode(brevKode: String?): DokumentBrevkode = values().first { it.verdi == brevKode }
+        fun fraBrevkode(brevKode: String?): DokumentBrevkode? = entries.firstOrNull { it.verdi == brevKode }
     }
 }
