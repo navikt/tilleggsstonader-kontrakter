0604897b160c627ac411bb06f3b7c88364d99d6a
Johan Blomgren <blommish@users.noreply.github.com>
2025-03-31 11:27:23 +0200

    Endrer til const

--- src/test/kotlin/no/nav/tilleggsstonader/kontrakter/FileUtil.kt
+++ src/test/kotlin/no/nav/tilleggsstonader/kontrakter/FileUtil.kt
@@ -6,7 +6,7 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 object FileUtil {
-    val SKAL_SKRIVE_TIL_FIL = false
+    const val SKAL_SKRIVE_TIL_FIL = false
 
     fun readFile(filnavn: String): String =
         FileUtil::class.java.classLoader
