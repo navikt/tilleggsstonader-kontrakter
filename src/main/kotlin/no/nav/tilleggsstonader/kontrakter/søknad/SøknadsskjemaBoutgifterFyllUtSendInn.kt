0a90d4fdf615cc0f49079eba1486f0558da7cbde
Johan Blomgren <blommish@users.noreply.github.com>
2025-04-04 11:48:54 +0200

    Tar med språk i kontraktet for skjema for boutgifter

--- src/main/kotlin/no/nav/tilleggsstonader/kontrakter/søknad/SøknadsskjemaBoutgifterFyllUtSendInn.kt
+++ src/main/kotlin/no/nav/tilleggsstonader/kontrakter/søknad/SøknadsskjemaBoutgifterFyllUtSendInn.kt
@@ -1,14 +1,13 @@
 package no.nav.tilleggsstonader.kontrakter.søknad
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import no.nav.tilleggsstonader.kontrakter.søknad.boutgifter.fyllutsendinn.BoutgifterFyllUtSendInnData
 
 /**
  * Søknadskjema som sendes inn fra FyllUt/SendInn
  * @param dokumentasjon fylles ikke i, men er påkrevd av [Skjema]
  */
-@JsonIgnoreProperties("language", ignoreUnknown = false)
 data class SøknadsskjemaBoutgifterFyllUtSendInn(
+    val language: String,
     val data: BoutgifterFyllUtSendInnData,
     override val dokumentasjon: List<DokumentasjonFelt> = emptyList(),
 ) : Skjema
