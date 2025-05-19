4d74cde648c388e8476dd6a7063b10fa2403621a
Andreas Strand <andreas.strand@nav.no>
2025-05-19 23:21:36 +0200

    Omstrukturerer til et multimodul-prosjekt

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,7 @@
-rootProject.name = "tilleggsstonader-kontrakter"
+// rootProject.name = "tilleggsstonader-kontrakter"
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
 }
+
+include("tilleggsstonader-kontrakter")
