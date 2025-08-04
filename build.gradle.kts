ec17d95fed2763656779f4baebf3b35140e7200c
Andreas Strand <andreas.strand@nav.no>
2025-08-04 12:30:40 +0200

    Ikke eksplisitt versjon på junit-jupiter

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ subprojects {
 
     dependencies {
         testImplementation(platform("org.junit:junit-bom:5.13.4"))
-        testImplementation("org.junit.jupiter:junit-jupiter:5.13.4")
+        testImplementation("org.junit.jupiter:junit-jupiter")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
 
