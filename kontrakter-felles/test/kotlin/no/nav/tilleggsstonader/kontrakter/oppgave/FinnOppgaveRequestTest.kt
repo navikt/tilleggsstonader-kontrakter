21cbf59d32b9809b179fa11de9cd957fd0acfc1c
Sigmund Berbom <sberbom@gmail.com>
2025-08-19 11:18:46 +0200

    ♻️ Bruk `accept_singel_value_as_array` i stedet for versjonering for å være bakover kompatibel

--- kontrakter-felles/test/kotlin/no/nav/tilleggsstonader/kontrakter/oppgave/FinnOppgaveRequestTest.kt
+++ kontrakter-felles/test/kotlin/no/nav/tilleggsstonader/kontrakter/oppgave/FinnOppgaveRequestTest.kt
@@ -11,7 +11,7 @@ class FinnOppgaveRequestTest {
         val finnOppgaveRequestString =
             """
             {
-              "tema": "TSO",
+              "tema": ["TSO","TSR"],
               "behandlingstema": "ab0300",
               "behandlingstype": "NASJONAL",
               "erUtenMappe": false,
@@ -44,11 +44,39 @@ class FinnOppgaveRequestTest {
     }
 
     @Test
-    fun `Kan konvertere fra json til FinnOppgaveRequestV2`() {
+    fun `Kan konvertere fra json til FinnOppgaveRequest med ett tema`() {
         val finnOppgaveRequestString =
             """
             {
-              "tema": ["TSO","TSR"],
+              "tema": "TSO",
+              "behandlingstema": "ab0300",
+              "behandlingstype": "NASJONAL",
+              "erUtenMappe": false,
+              "oppgavetype": "BehandleSak",
+              "enhet": "1234",
+              "saksbehandler": "John Smith",
+              "aktørId": "12345678",
+              "journalpostId": "J123456",
+              "saksreferanse": "S123456",
+              "tilordnetRessurs": "Jane Smith",
+              "tildeltRessurs": true,
+              "opprettetFomTidspunkt": "2023-01-12T08:21:16.290013",
+              "opprettetTomTidspunkt": "2023-01-12T08:21:16.290013",
+              "fristFomDato": "2000-10-31",
+              "fristTomDato": "2000-10-31",
+              "aktivFomDato": "2000-10-31",
+              "aktivTomDato": "2000-10-31",
+              "mappeId": 12345,
+              "limit" : 10,
+              "offset" : 0,
+              "sorteringsrekkefolge" : "ASC",
+              "sorteringsfelt" : "FRIST"
+            }
+            """.trimIndent()
+        val finnOppgaveRequestForventetString =
+            """
+            {
+              "tema": ["TSO"],
               "behandlingstema": "ab0300",
               "behandlingstype": "NASJONAL",
               "erUtenMappe": false,
@@ -74,9 +102,9 @@ class FinnOppgaveRequestTest {
             }
             """.trimIndent()
 
-        val request = objectMapper.readValue<FinnOppgaveRequestV2>(finnOppgaveRequestString)
+        val request = objectMapper.readValue<FinnOppgaveRequest>(finnOppgaveRequestString)
         val requestJson = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(request)
 
-        assertThat(requestJson).isEqualToIgnoringWhitespace(finnOppgaveRequestString)
+        assertThat(requestJson).isEqualToIgnoringWhitespace(finnOppgaveRequestForventetString)
     }
 }
