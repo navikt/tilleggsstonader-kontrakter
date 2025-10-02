58856917dc950ddbf2c0bcd2a1c360c46bfca155
Andreas Strand <andreas.strand@nav.no>
2025-10-02 10:46:41 +0200

    Fjern HåndterSøknadRequest som ikke er i bruk lenger da sak mottar søknader fra journalhendelser kafka-topic

--- kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/sak/journalføring/HåndterSøknadRequest.kt
+++
@@ -1,9 +0,0 @@
-package no.nav.tilleggsstonader.kontrakter.sak.journalføring
-
-import no.nav.tilleggsstonader.kontrakter.felles.Stønadstype
-
-data class HåndterSøknadRequest(
-    val personIdent: String,
-    val journalpostId: String,
-    val stønadstype: Stønadstype,
-)
