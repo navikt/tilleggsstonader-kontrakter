8d7e483d0404f665c7e2aa21339dabf679da5f5c
Andreas Strand <andreas.strand@nav.no>
2025-10-08 13:06:25 +0200

    Fjern gamle koder for Skjematype

--- kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/Skjematype.kt
+++ kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/Skjematype.kt
@@ -1,34 +1,30 @@
 package no.nav.tilleggsstonader.kontrakter.felles
 
 enum class Skjematype {
-    BARNETILSYN,
     SØKNAD_BARNETILSYN,
-    LÆREMIDLER,
     SØKNAD_LÆREMIDLER,
-    BOUTGIFTER,
     SØKNAD_BOUTGIFTER,
-    DAGLIG_REISE,
     SØKNAD_DAGLIG_REISE,
     DAGLIG_REISE_KJØRELISTE,
 }
 
 fun Stønadstype.tilSkjematype() =
     when (this) {
-        Stønadstype.BARNETILSYN -> Skjematype.BARNETILSYN
-        Stønadstype.LÆREMIDLER -> Skjematype.LÆREMIDLER
-        Stønadstype.BOUTGIFTER -> Skjematype.BOUTGIFTER
-        Stønadstype.DAGLIG_REISE_TSO, Stønadstype.DAGLIG_REISE_TSR -> Skjematype.DAGLIG_REISE
+        Stønadstype.BARNETILSYN -> Skjematype.SØKNAD_BARNETILSYN
+        Stønadstype.LÆREMIDLER -> Skjematype.SØKNAD_LÆREMIDLER
+        Stønadstype.BOUTGIFTER -> Skjematype.SØKNAD_BOUTGIFTER
+        Stønadstype.DAGLIG_REISE_TSO, Stønadstype.DAGLIG_REISE_TSR -> Skjematype.SØKNAD_DAGLIG_REISE
     }
 
 fun Skjematype.tilStønadstyper(): Set<Stønadstype> =
     when (this) {
-        Skjematype.DAGLIG_REISE, Skjematype.SØKNAD_DAGLIG_REISE, Skjematype.DAGLIG_REISE_KJØRELISTE ->
+        Skjematype.SØKNAD_DAGLIG_REISE, Skjematype.DAGLIG_REISE_KJØRELISTE ->
             setOf(
                 Stønadstype.DAGLIG_REISE_TSO,
                 Stønadstype.DAGLIG_REISE_TSR,
             )
 
-        Skjematype.BOUTGIFTER, Skjematype.SØKNAD_BOUTGIFTER -> setOf(Stønadstype.BOUTGIFTER)
-        Skjematype.LÆREMIDLER, Skjematype.SØKNAD_LÆREMIDLER -> setOf(Stønadstype.LÆREMIDLER)
-        Skjematype.BARNETILSYN, Skjematype.SØKNAD_BARNETILSYN -> setOf(Stønadstype.BARNETILSYN)
+        Skjematype.SØKNAD_BOUTGIFTER -> setOf(Stønadstype.BOUTGIFTER)
+        Skjematype.SØKNAD_LÆREMIDLER -> setOf(Stønadstype.LÆREMIDLER)
+        Skjematype.SØKNAD_BARNETILSYN -> setOf(Stønadstype.BARNETILSYN)
     }
