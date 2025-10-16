8c6104b4922954e92297126626d5cefc9a0d4ce8
Andreas Strand <andreas.strand@nav.no>
2025-10-16 11:30:58 +0200

    Legg inn brevkoder for gamle søknader

--- kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/sak/DokumentBrevkode.kt
+++ kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/sak/DokumentBrevkode.kt
@@ -4,10 +4,15 @@ enum class DokumentBrevkode(
     val verdi: String,
 ) {
     BARNETILSYN("NAV 11-12.15"),
+    BARNETILSYN_GAMMEL("NAV 11-12.15b"),
     LÆREMIDLER("NAV 11-12.16"),
+    LÆREMIDLER_GAMMEL("NAV 11-12.16b"),
     BOUTGIFTER("NAV 11-12.19"),
+    BOUTGIFTER_GAMMEL("NAV 11-12.19b"),
     DAGLIG_REISE("NAV 11-12.21"),
+    DAGLIG_REISE_GAMMEL("NAV 11-12.21b"),
     DAGLIG_REISE_KJØRELISTE("NAV 11-12.24"),
+    DAGLIG_REISE_KJØRELISTE_GAMMEL("NAV 11-12.24b"),
     ;
 
     companion object {
