b64ed7fd4933aa7333dc817f42aa7b53030f6b04
Andreas Strand <andreas.strand@nav.no>
2025-11-04 10:22:27 +0100

    Legg til enum Enhet og en extension-function på Stønadstype som gir tilhørende enhet for en gitt stønadstype (#150)

---
+++ kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/Enhet.kt
@@ -0,0 +1,8 @@
+package no.nav.tilleggsstonader.kontrakter.felles
+
+enum class Enhet(
+    val enhetsnr: String,
+) {
+    NAV_ARBEID_OG_YTELSER_TILLEGGSSTØNAD("4462"),
+    NAV_TILTAK_OSLO("0387"),
+}
