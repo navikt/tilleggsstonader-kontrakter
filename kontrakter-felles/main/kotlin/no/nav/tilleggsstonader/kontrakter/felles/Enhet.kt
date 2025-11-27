29b9e7f1339a50805d8b53eea21dec20b23fd95b
Andreas Strand <andreas.strand@nav.no>
2025-11-27 22:36:31 +0100

    Utvider enum Enhet med NAY Egne Ansatte og Vikafossen

--- kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/Enhet.kt
+++ kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/Enhet.kt
@@ -4,5 +4,7 @@ enum class Enhet(
     val enhetsnr: String,
 ) {
     NAV_ARBEID_OG_YTELSER_TILLEGGSSTØNAD("4462"),
+    NAV_ARBEID_OG_YTELSER_EGNE_ANSATTE("4483"),
     NAV_TILTAK_OSLO("0387"),
+    VIKAFOSSEN("2103"),
 }
