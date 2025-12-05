728aebddf266a6b9c0689563c4b4dd0dad0969f2
Andreas Strand <andreas.strand@nav.no>
2025-12-05 11:51:05 +0100

    Oppdaterer til jackson 3

--- kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/ObjectMapperProvider.kt
+++ kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/felles/ObjectMapperProvider.kt
@@ -1,38 +1,23 @@
 package no.nav.tilleggsstonader.kontrakter.felles
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.datatype.jsr310.deser.YearMonthDeserializer
-import com.fasterxml.jackson.module.kotlin.addDeserializer
-import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import java.time.YearMonth
-import java.time.format.DateTimeFormatter
+import tools.jackson.databind.DeserializationFeature
+import tools.jackson.databind.json.JsonMapper
+import tools.jackson.module.kotlin.KotlinFeature
+import tools.jackson.module.kotlin.jacksonMapperBuilder
 
 object ObjectMapperProvider {
-    val javaTimeModule =
-        JavaTimeModule()
-            .addDeserializer(
-                YearMonth::class,
-                YearMonthDeserializer(DateTimeFormatter.ofPattern("u-MM")), // Denne trengs for å parse år over 9999 riktig.
-            )
-
-    private fun lagObjectMapper() =
-        ObjectMapper()
-            .registerKotlinModule()
-            .registerModule(Jdk8Module())
-            .registerModule(javaTimeModule)
-            .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
-            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+    // Uten KotlinPropertyNameAsImplicitName så vil ikke properties som starter med non-ascii bokstaver blir fjernet
+    private fun lagJsonMapper() =
+        jacksonMapperBuilder { configure(KotlinFeature.KotlinPropertyNameAsImplicitName, true) }
             .enable(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
 
-    val objectMapper: ObjectMapper =
-        lagObjectMapper()
+    val jsonMapper: JsonMapper =
+        lagJsonMapper()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .build()
 
-    val objectMapperFailOnUnknownProperties =
-        lagObjectMapper()
+    val jsonMapperFailOnUnknownProperties: JsonMapper =
+        lagJsonMapper()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true)
+            .build()
 }
