cfd51f17728a8c25661d8d67065629506679890a
Sigmund Berbom <sberbom@gmail.com>
2026-02-24 11:12:08 +0100

    👔 Legg til dato for når gjennstående dager fra telleverket er regnet ut.
    For etterlevelse og informasjon til saksbehandler

--- kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/ytelse/YtelsePerioderDto.kt
+++ kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/ytelse/YtelsePerioderDto.kt
@@ -41,7 +41,7 @@ data class YtelsePeriode(
     val tom: LocalDate?,
     val aapErFerdigAvklart: Boolean? = null,
     val ensligForsørgerStønadstype: EnsligForsørgerStønadstype? = null,
-    val gjennståendeDagerFraTelleverk: Int? = null,
+    val gjennståendeDagerFraTelleverk: GjennståendeDagerFraTelleverk? = null,
 ) {
     init {
         if (type != TypeYtelsePeriode.AAP && aapErFerdigAvklart != null) {
@@ -52,6 +52,11 @@ data class YtelsePeriode(
         }
     }
 }
+
+data class GjennståendeDagerFraTelleverk(
+    val dato: LocalDate,
+    val antallDager: Int,
+)
 
 enum class TypeYtelsePeriode {
     AAP,
