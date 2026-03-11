c0a4c6977460669af871d471f0ed6a130d448ba1
Sigmund Berbom <sberbom@gmail.com>
2026-03-11 10:39:38 +0100

    🏷️ Legg til RammevedtakDto som skal brukes mellom sak og soknad-api

---
+++ kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/søknad/Rammevedtak.kt
@@ -0,0 +1,21 @@
+package no.nav.tilleggsstonader.kontrakter.søknad
+
+import java.time.LocalDate
+
+data class RammevedtakDto(
+    val reiseId: String,
+    val fom: LocalDate,
+    val tom: LocalDate,
+    val reisedagerPerUke: Int,
+    val aktivitetsadresse: String,
+    val aktivitetsnavn: String,
+    val uker: List<RammevedtakUkeDto>,
+)
+
+data class RammevedtakUkeDto(
+    val fom: LocalDate,
+    val tom: LocalDate,
+    val ukeNummer: Int,
+    val innsendtDato: LocalDate?,
+    val kanSendeInnKjøreliste: Boolean,
+)
