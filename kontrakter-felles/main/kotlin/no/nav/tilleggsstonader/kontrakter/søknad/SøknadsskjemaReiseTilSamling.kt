ced35659c426ef46a463958a4de32e01b19c45c2
Adnan Vrevic <adnan.vrevic@nav.no>
2026-06-03 15:18:02 +0200

    Legger til dataklasser for reise til samling i søknadsskjema

--- kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/søknad/SøknadsskjemaReiseTilSamling.kt
+++ kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/søknad/SøknadsskjemaReiseTilSamling.kt
@@ -1,31 +1,16 @@
 package no.nav.tilleggsstonader.kontrakter.søknad
 
-import no.nav.tilleggsstonader.kontrakter.søknad.barnetilsyn.AktivitetAvsnitt
 import no.nav.tilleggsstonader.kontrakter.søknad.felles.HovedytelseAvsnitt
+import no.nav.tilleggsstonader.kontrakter.søknad.reisetilsamling.AktivitetAvsnitt
+import no.nav.tilleggsstonader.kontrakter.søknad.reisetilsamling.ReiseavstandAvsnitt
+import no.nav.tilleggsstonader.kontrakter.søknad.reisetilsamling.ReisemåteAvsnitt
+import no.nav.tilleggsstonader.kontrakter.søknad.reisetilsamling.Samling
 
 data class SøknadsskjemaReiseTilSamling(
     val hovedytelse: HovedytelseAvsnitt,
     val aktivitet: AktivitetAvsnitt,
-    val samlinger: List<PeriodeFelt>?,
-    val oppmøteadresse: AdresseFelt?,
-    val kanReiseKollektivt: EnumFelt<JaNei>?,
-    val totalbeløpKollektivt: HeltallFelt?,
-    val årsakIkkeKollektivt: EnumFelt<ÅrsakIkkeKollektivt>?,
-    val kanBenytteEgenBil: EnumFelt<JaNei>?,
-    val årsakIkkeEgenBil: EnumFelt<ÅrsakIkkeEgenBil>?,
-    val kanBenytteDrosje: EnumFelt<JaNei>?,
+    val samlinger: List<Samling>,
+    val reiseavstand: ReiseavstandAvsnitt,
+    val reisemåte: ReisemåteAvsnitt,
     override val dokumentasjon: List<DokumentasjonFelt>,
-) : Skjemadata {
-    enum class ÅrsakIkkeKollektivt {
-        HELSEMESSIGE_ÅRSAKER,
-        DÅRLIG_TRANSPORTTILBUD,
-        HENTING_LEVERING_BARN,
-        ANNET,
-    }
-
-    enum class ÅrsakIkkeEgenBil {
-        HELSEMESSIGE_ÅRSAKER,
-        DISPONERER_IKKE_BIL,
-        ANNET,
-    }
-}
+) : Skjemadata
---
+++ kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/søknad/reisetilsamling/AvsnittReiseTilSamling.kt
@@ -0,0 +1,39 @@
+package no.nav.tilleggsstonader.kontrakter.søknad.reisetilsamling
+
+import no.nav.tilleggsstonader.kontrakter.søknad.DatoFelt
+import no.nav.tilleggsstonader.kontrakter.søknad.EnumFelt
+import no.nav.tilleggsstonader.kontrakter.søknad.EnumFlereValgFelt
+import no.nav.tilleggsstonader.kontrakter.søknad.JaNei
+import no.nav.tilleggsstonader.kontrakter.søknad.SelectFelt
+import no.nav.tilleggsstonader.kontrakter.søknad.VerdiFelt
+import no.nav.tilleggsstonader.kontrakter.søknad.barnetilsyn.AnnenAktivitetType
+
+data class AktivitetAvsnitt(
+    val aktiviteter: EnumFlereValgFelt<String>?,
+    val annenAktivitet: EnumFelt<AnnenAktivitetType>?,
+    val lønnetAktivitet: EnumFelt<JaNei>?,
+)
+
+data class Samling(
+    val fom: DatoFelt?,
+    val tom: DatoFelt?,
+)
+
+data class AktivitetsadresseAvsnitt(
+    val land: SelectFelt<String>?,
+    val gateadresse: VerdiFelt<String>?,
+    val postnummer: VerdiFelt<String>?,
+    val poststed: VerdiFelt<String>?,
+)
+
+data class ReiseavstandAvsnitt(
+    val antallKilometerEnVei: VerdiFelt<String>?,
+    val aktivitetsadresse: AktivitetsadresseAvsnitt,
+)
+
+data class ReisemåteAvsnitt(
+    val kanReiseKollektivt: EnumFelt<JaNei>?,
+    val totalutgifterKollektivt: VerdiFelt<String>?,
+    val kanBenytteEgenBil: EnumFelt<JaNei>?,
+    val kanBenytteDrosje: EnumFelt<JaNei>?,
+)
