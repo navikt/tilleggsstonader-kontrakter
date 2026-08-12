7c5db548d32f776bd530741b7d20a57102adb31f
anderssonw <wandersson.98@gmail.com>
2026-08-12 10:42:09 +0200

    legg tilbake nullability på oppgave

--- kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/oppgave/Oppgave.kt
+++ kontrakter-felles/main/kotlin/no/nav/tilleggsstonader/kontrakter/oppgave/Oppgave.kt
@@ -19,13 +19,13 @@ data class OppdatertOppgaveResponse(
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class Oppgave(
     val id: Long,
-    val tildeltEnhetsnr: String,
     val versjon: Int,
-    val tema: Tema,
-    val oppgavetype: String,
-    val prioritet: OppgavePrioritet,
-    val status: StatusEnum,
-    val aktivDato: LocalDate,
+    val tildeltEnhetsnr: String? = null,
+    val tema: Tema? = null,
+    val oppgavetype: String? = null,
+    val prioritet: OppgavePrioritet? = null,
+    val status: StatusEnum? = null,
+    val aktivDato: LocalDate? = null,
     @Deprecated("Skal ikke brukes, foretrekk bruker og OppgaveBruker")
     val identer: List<OppgaveIdentV2>? = null,
     val bruker: OppgaveBruker? = null,
